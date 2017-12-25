8abe4c97d163ed3ccf4f8baa72cb7396f689b392
user <webmaster@szafirek.art.pl>
2017-12-25 17:22:00 +0100

    test

--- test.ino
+++ test.ino
@@ -1,17 +1,21 @@
 #include <SoftwareSerial.h>
 
+int rawData = 0;
 SoftwareSerial mySerial(A2, A1); // RX, TX
 
 void setup() {
   Serial.begin (9600);
   mySerial.begin(9600);
   
+     Serial.println("Przeszetem setup");
 }
 
 void loop() {
-  String rawData;
+  
   if (mySerial.available()> 0) {
+    Serial.println("mySerial available");
     rawData = mySerial.read();
+    Serial.println("SKonczylem odbierac mySerial");
     Serial.println(rawData);
   }
   else {

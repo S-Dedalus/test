e0fd9bee2962cb57a8b3fcab8f40da526d69fb5b
user <webmaster@szafirek.art.pl>
2017-12-25 17:00:53 +0100

    test

--- test.ino
+++ test.ino
@@ -1,39 +1,18 @@
-#define trigPin A1
-#define echoPin A2
-#define ledRedPin 11
-#define ledGreenPin 10
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(A2, A1); // RX, TX
 
 void setup() {
   Serial.begin (9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT_PULLUP);
-  pinMode(ledRedPin, OUTPUT);
-  pinMode(ledGreenPin, OUTPUT);
+  mySerial.begin(9600);
+  
 }
 
 void loop() {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10); 
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  if (distance < 4) {  // comparison for led ON/OFF
-    digitalWrite(ledRedPin,HIGH); // when Red LED turns ON, Green LED turns off
-  digitalWrite(ledGreenPin,LOW);
-}
-  else {
-    digitalWrite(ledRedPin,LOW);
-    digitalWrite(ledGreenPin,HIGH);
+  String rawData;
+  if (mySerial.available()) {
+    rawData = Serial.readStringUntil(0xFF);
   }
-  if (distance >= 400 || distance <= 0){
-    Serial.println("Out of range");
-  }
-  else {
-    Serial.print(distance);
-    Serial.println(" cm");
-  }
+  Serial.println(rawData);
   delay(500);
 }

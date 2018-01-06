4aa6554bd2e6bdf219d588ebc7655929a304c277
user <webmaster@szafirek.art.pl>
2018-01-06 10:29:39 +0100

    test

--- test.ino
+++ test.ino
@@ -8,35 +8,18 @@ void setup()
 {
 Serial.begin(9600);
 pinMode(ultraSoundSignal,OUTPUT);
-pinMode(ultraSoundSignalIN,INPUT);
+pinMode(ultraSoundSignalIN,INPUT_PULLUP);
 
 }
 
-unsigned long ping(){
-pinMode(ultraSoundSignal, OUTPUT); // Switch signalpin to output
-digitalWrite(ultraSoundSignal, LOW); // Send low pulse
-delayMicroseconds(2); // Wait for 2 microseconds
-digitalWrite(ultraSoundSignal, HIGH); // Send high pulse
-delayMicroseconds(15); // Wait for 15 microseconds
-digitalWrite(ultraSoundSignal, LOW); // Holdoff
-pinMode(ultraSoundSignalIN, INPUT); // Switch signalpin to input
-digitalWrite(ultraSoundSignalIN, HIGH); // Turn on pullup resistor
-// please note that pulseIn has a 1sec timeout, which may
-// not be desirable. Depending on your sensor specs, you
-// can likely bound the time like this — marcmerlin
-// echo = pulseIn(ultraSoundSignal, HIGH, 38000)
-echo = pulseIn(ultraSoundSignalIN, HIGH); //Listen for echo
-Serial.println(echo);
-ultrasoundValue = (echo / 58.138) * .39; //convert to CM then to inches
-return ultrasoundValue;
 
-}
+
+
 
 void loop()
 {
-int x = 0;
-x = ping();
-//Serial.println(x);
-delay(250); //delay 1/4 seconds.
+int one;
+one = Serial.read(); 
+Serial.print(one);
 
 }

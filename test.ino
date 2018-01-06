e1049778fde6891bf4c7524d08d1fd33132db132
user <webmaster@szafirek.art.pl>
2018-01-06 15:01:54 +0100

    test

--- test.ino
+++ test.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 
 byte rawData0 = 0;
-byte rawData1 = 0;
+uint16_t rawData1 = 0;
 byte rawData2 = 0;
 byte rawData3 = 0;
 uint16_t dist = 0;
@@ -15,34 +15,34 @@ void setup() {
   Serial.begin (9600);
   mySerial.begin(9600);
   
-     Serial.println("Przeszetem setup");
+     
 }
 
 void loop() {
   
   if (mySerial.available()> 0) {
     rawData0 = mySerial.read();
-    Serial.print(rawData0, HEX);
-    Serial.print("|");
+//    Serial.print(rawData0, HEX);
+//    Serial.print("|");
     rawData1 = mySerial.read();
-    Serial.print(rawData1, HEX);
-    Serial.print("|");
+//    Serial.print(rawData1, HEX);
+//    Serial.print("|");
     rawData2 = mySerial.read();
-    Serial.print(rawData2, HEX);
-    Serial.print("|");
+//    Serial.print(rawData2, HEX);
+//    Serial.print("|");
     rawData3 = mySerial.read();
-    Serial.print(rawData3, HEX);
-    Serial.println("|");
+//    Serial.print(rawData3, HEX);
+//    Serial.println("|");
      long one = long(SumaCal);
-     Serial.println(one, HEX);
-     uint16_t(rawdata1);
+//     Serial.println(one, HEX);
      rawData1 = rawData1 << 8;
      dist = (rawData1 + rawData2);
-     Serial.print(rawData1, BIN);
-     Serial.print("|");
-     Serial.println(rawData2, BIN);
-     Serial.println(dist, BIN);
-     Serial.println(dist);
+//     Serial.print(rawData1, BIN);
+//     Serial.print("|");
+//     Serial.println(rawData2, BIN);
+//     Serial.println(dist, BIN);
+     Serial.print(dist/10);
+     Serial.println(" cm");
      
      delay(500);
   }
